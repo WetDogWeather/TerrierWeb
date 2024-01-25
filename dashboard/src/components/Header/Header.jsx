3996c7c5f59d3eb9bce109cc5746bdf095283620
Steve Gifford <sjg@mousebirdconsulting.com>
2024-01-24 16:54:15 -0800

    Tweaked logo placement

--- dashboard/src/components/Header/Header.jsx
+++ dashboard/src/components/Header/Header.jsx
@@ -6,9 +6,14 @@ function Header(props) {
     return (
         <>
             <nav className="header">
-                <img id='logo' src={zeusLogo} alt="Zeus.ai Logo" />
-                <h1 id='splash-text'>LENS-Cast</h1>
-                <p id='splash-text'>  Visualization by Wet Dog Weather</p>
+                    <img id='logo' src={zeusLogo} alt="Zeus.ai Logo" /> 
+                    <h1 id='splash-text'>LENS-Cast</h1>
+                    <div>
+                    <h3 id='splash-text2'>Visualization by </h3>
+                    <a href="https://www.wetdogweather.com/">
+                    <img id='logo2' src={wdwLogo} height='40px' />
+                    </a>
+                    </div>
                 <ul className="header-nav"> {props.children} </ul>
             </nav>
         </>

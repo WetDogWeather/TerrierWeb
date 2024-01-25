92059848912c11a13ae3d7fbba7730a58c7dbb7e
Steve Gifford <sjg@mousebirdconsulting.com>
2024-01-24 16:23:53 -0800

    Added Zeus/Lens-cast logo

--- dashboard/src/components/Header/Header.jsx
+++ dashboard/src/components/Header/Header.jsx
@@ -1,12 +1,14 @@
 import wdwLogo from '../../assets/wdw.png'
+import zeusLogo from '../../assets/zeusLogo.png'
 import './header.css'
 
 function Header(props) {
     return (
         <>
             <nav className="header">
-                <img id='logo' src={wdwLogo} alt="Wet Dog Weather Logo" />
-                <h1 id='splash-text'>Terrier + MapLibre</h1>
+                <img id='logo' src={zeusLogo} alt="Zeus.ai Logo" />
+                <h1 id='splash-text'>LENS-Cast</h1>
+                <p id='splash-text'>  Visualization by Wet Dog Weather</p>
                 <ul className="header-nav"> {props.children} </ul>
             </nav>
         </>

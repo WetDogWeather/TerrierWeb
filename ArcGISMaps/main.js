02f7b5ff3409c9e5ddfb88ed059e2f74efcb09c8
Steve Gifford <sjg@mousebirdconsulting.com>
2024-10-10 17:09:37 -0700

    Testing the ArcGIS integration a bit more.

--- ArcGISMaps/main.js
+++ ArcGISMaps/main.js
@@ -25,7 +25,7 @@ const view = new MapView({
 });
 
 Terrier.startArcGIS("dev", view, (ovl) => {
-  let tempLayer = ovl.startLayer('radar', {
+  let tempLayer = ovl.startLayer('temperature', {
       // colorMap: {}
       interpMode: 'linear',
       opacity: 0.5,
@@ -33,6 +33,21 @@ Terrier.startArcGIS("dev", view, (ovl) => {
       cadence: [-4*60*60,0,64]
   })
 
+  setTimeout(() => {
+    ovl.stopLayer(tempLayer)
+
+    setTimeout(() => {
+      let tempLayer = ovl.startLayer('temperature', {
+          // colorMap: {}
+          interpMode: 'linear',
+          opacity: 0.5,
+          // Four hours worth of past radar, maximum of 64 frames
+          cadence: [-4*60*60,0,64]
+        })
+      }, 5000)
+  }, 10000)
+
+
   // Turn this on to animate over time
-  // ovl.timePlay({period: 20.0})    
+  ovl.timePlay({period: 20.0})    
 })
